d36dd300a616939824f50d02e3117b2031aef605
Stefan Haller <fgrsnau@gmail.com>
2017-11-07 23:01:55 +0100

    Replace dict by record

--- pythonx/cm_sources/cm_otherbufkeyword.py
+++ pythonx/cm_sources/cm_otherbufkeyword.py
@@ -16,6 +16,14 @@ logger = getLogger(__name__)
 
 class Source(Base):
 
+    class BufferData:
+        __sltos__ = ('changed', 'changedtick', 'deleted', 'words')
+
+        def __init__(self):
+            self.changed = False
+            self.deleted = False
+            self.words = set()
+
     def __init__(self, nvim):
         super(Source,self).__init__(nvim)
         self._compiled = re.compile(r'\w+')
@@ -24,31 +32,32 @@ class Source(Base):
 
     def update(self):
         for buf in self._buffers.values():
-            buf['deleted'] = True
+            buf.deleted = True
 
         for buf in self.nvim.buffers:
             schedule_update = (not buf.number in self._buffers) or \
-                (self._buffers[buf.number]['changed'])
+                (self._buffers[buf.number].changed)
             if schedule_update:
                 self._buffers[buf.number] = self.rescan_buffer(buf)
-            self._buffers[buf.number]['deleted'] = False
+            self._buffers[buf.number].deleted = False
 
-        self._buffers = { k:v for k, v in self._buffers.items() if not v['deleted'] }
+        self._buffers = { k:v for k, v in self._buffers.items() if not v.deleted }
 
     def rescan_buffer(self, buf):
-        result = { 'changed': False, 'words': set() }
+        result = self.BufferData()
         logger.info('rescan_buffer(%s)', buf.number)
 
         for line in buf:
             for word in self._compiled.finditer(line):
-                result['words'].add(word.group())
+                result.words.add(word.group())
 
-        logger.info('keyword refresh complete, count: %s', len(result['words']))
+        logger.info('keyword refresh complete, count: %s', len(result.words))
         return result
 
     def cm_event(self, event, ctx, *args):
         if event == 'BufLeave':
-            self._buffers[ctx['bufnr']]['changed'] = True
+            buf = self._buffers[ctx['bufnr']]
+            buf.changed = True
         self.update()
 
     def cm_refresh(self, info, ctx):
@@ -56,7 +65,7 @@ class Source(Base):
             for num, buf in self._buffers.items():
                 # current buffer is handled by different source
                 if num != ctx['bufnr']:
-                    for word in buf['words']:
+                    for word in buf.words:
                         yield word
 
         matches = (dict(word=word, icase=1) for word in gen())
